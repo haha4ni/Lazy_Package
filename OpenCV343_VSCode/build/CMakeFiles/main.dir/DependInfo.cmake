fdea46723682eae35cf5840207273d40b003299e
Haha <haha4ni@hotmail.com>
2020-06-11 20:21:58 +0800

    commit_1

---
+++ OpenCV343_VSCode/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Haha/Desktop/test/Lazy_Package/OpenCV343_VSCode/src/main.cpp" "C:/Users/Haha/Desktop/test/Lazy_Package/OpenCV343_VSCode/build/CMakeFiles/main.dir/C_/Users/Haha/Desktop/test/Lazy_Package/OpenCV343_VSCode/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../OpenCV343/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpenCV343_VSCode/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "C:/Users/Haha/Desktop/test/Lazy_Package/OpenCV343_VSCode/bin/libmain.dll.a"
+  "C:/Users/Haha/Desktop/test/Lazy_Package/OpenCV343_VSCode/bin/main.exe"
+  "C:/Users/Haha/Desktop/test/Lazy_Package/OpenCV343_VSCode/bin/main.exe.manifest"
+  "C:/Users/Haha/Desktop/test/Lazy_Package/OpenCV343_VSCode/bin/main.pdb"
+  "CMakeFiles/main.dir/C_/Users/Haha/Desktop/test/Lazy_Package/OpenCV343_VSCode/src/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
